08eb54d6030b7cdfc6ae000c7e770266b5c6e8fc
Average454 <127059667+Average454@users.noreply.github.com>
2023-03-06 21:12:40 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,21 +5,21 @@ from discord.ext import commands
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 intents.message_content = True
-TOKEN = os.environ['TOKEN']
-work_channel = os.environ['work_channel']
-start_channel = os.environ['start_channel']
-Target_ID = os.environ['Target_ID']
+_TOKEN = os.environ['TOKEN']
+_WORK_CHANNEL = os.environ['work_channel']
+_START_CHANNEL = os.environ['start_channel']
+_TARGET_ID = os.environ['Target_ID']
 
 @bot.event
 async def on_ready():
     print('Bot is ready.')
-    channel = bot.get_channel(start_channel)
+    channel = bot.get_channel(_START_CHANNEL)
     await channel.send("開始工作")
 @bot.event
 async def on_message(message):
-    channel = bot.get_channel(work_channel)
+    channel = bot.get_channel(_WORK_CHANNEL)
     print(message.content)
-    if message.author.id == Target_ID:
+    if message.author.id == _TARGET_ID:
         if message.attachments:
             print("傳了一張圖片")
             # 逐一處理圖片並傳送到頻道中
@@ -35,4 +35,4 @@ async def on_message(message):
             await message.channel.send(f"{message.author.mention} 傳送了以下訊息：")
             await channel.send(message.content)
       
-bot.run(TOKEN)
+bot.run(_TOKEN)
